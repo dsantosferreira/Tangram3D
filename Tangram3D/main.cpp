99704298ed5f9e8e4ac96b2d1029106a4b811759
Daniel Ferreira <unidsferreira2003@gmail.com>
2025-12-07 11:04:23 +0000

    chore: deleted unnecessary code

--- Tangram3D/main.cpp
+++ Tangram3D/main.cpp
@@ -62,119 +62,13 @@ private:
     GLint ModelMatrixId;
 
     void createShaderPrograms();
-    void createBufferObjects();
     void createCameras();
     mgl::Mesh* getMesh(std::string mesh_dir, std::string mesh_file);
     void createSceneGraph();
     void setCurrentPositions(double elapsed);
     void destroyCameras();
-    void destroyBufferObjects();
     void drawScene(double elapsed);
 };
-
-////////////////////////////////////////////////////////////////// VAO, VBO, EBO
-
-typedef struct {
-    GLfloat XYZW[4];
-    GLfloat RGBA[4];
-} Vertex;
-
-const Vertex Vertices[] = {
-    {{0.0f, 0.0f, 1.0f, 1.0f}, {0.9f, 0.1f, 0.1f, 1.0f}}, // 0 - FRONT
-    {{1.0f, 0.0f, 1.0f, 1.0f}, {0.9f, 0.1f, 0.1f, 1.0f}}, // 1
-    {{1.0f, 1.0f, 1.0f, 1.0f}, {0.9f, 0.1f, 0.1f, 1.0f}}, // 2
-    // 2
-{{0.0f, 1.0f, 1.0f, 1.0f}, {0.9f, 0.1f, 0.1f, 1.0f}}, // 3
-// 0
-
-{{1.0f, 0.0f, 1.0f, 1.0f}, {0.1f, 0.9f, 0.1f, 1.0f}}, // 1 - RIGHT
-{{1.0f, 0.0f, 0.0f, 1.0f}, {0.1f, 0.9f, 0.1f, 1.0f}}, // 5
-{{1.0f, 1.0f, 0.0f, 1.0f}, {0.1f, 0.9f, 0.1f, 1.0f}}, // 6
-// 6
-{{1.0f, 1.0f, 1.0f, 1.0f}, {0.1f, 0.9f, 0.1f, 1.0f}}, // 2
-// 1
-
-{{1.0f, 1.0f, 1.0f, 1.0f}, {0.1f, 0.1f, 0.9f, 1.0f}}, // 2 - TOP
-{{1.0f, 1.0f, 0.0f, 1.0f}, {0.1f, 0.1f, 0.9f, 1.0f}}, // 6
-{{0.0f, 1.0f, 0.0f, 1.0f}, {0.1f, 0.1f, 0.9f, 1.0f}}, // 7
-// 7
-{{0.0f, 1.0f, 1.0f, 1.0f}, {0.1f, 0.1f, 0.9f, 1.0f}}, // 3
-// 2
-
-{{1.0f, 0.0f, 0.0f, 1.0f}, {0.1f, 0.9f, 0.9f, 1.0f}}, // 5 - BACK
-{{0.0f, 0.0f, 0.0f, 1.0f}, {0.1f, 0.9f, 0.9f, 1.0f}}, // 4
-{{0.0f, 1.0f, 0.0f, 1.0f}, {0.1f, 0.9f, 0.9f, 1.0f}}, // 7
-// 7
-{{1.0f, 1.0f, 0.0f, 1.0f}, {0.1f, 0.9f, 0.9f, 1.0f}}, // 6
-// 5
-
-{{0.0f, 0.0f, 0.0f, 1.0f}, {0.9f, 0.1f, 0.9f, 1.0f}}, // 4 - LEFT
-{{0.0f, 0.0f, 1.0f, 1.0f}, {0.9f, 0.1f, 0.9f, 1.0f}}, // 0
-{{0.0f, 1.0f, 1.0f, 1.0f}, {0.9f, 0.1f, 0.9f, 1.0f}}, // 3
-// 3
-{{0.0f, 1.0f, 0.0f, 1.0f}, {0.9f, 0.1f, 0.9f, 1.0f}}, // 7
-// 4
-
-{{0.0f, 0.0f, 1.0f, 1.0f}, {0.9f, 0.9f, 0.1f, 1.0f}}, // 0 - BOTTOM
-{{0.0f, 0.0f, 0.0f, 1.0f}, {0.9f, 0.9f, 0.1f, 1.0f}}, // 4
-{{1.0f, 0.0f, 0.0f, 1.0f}, {0.9f, 0.9f, 0.1f, 1.0f}}, // 5
-// 5
-{{1.0f, 0.0f, 1.0f, 1.0f}, {0.9f, 0.9f, 0.1f, 1.0f}}  // 1
-// 0
-};
-
-const unsigned int Indices[] = {
-    0,  1,  2,  2,  3,  0,  // FRONT
-    4,  5,  6,  6,  7,  4,  // RIGHT
-    8,  9,  10, 10, 11, 8,  // TOP
-    12, 13, 14, 14, 15, 12, // BACK
-    16, 17, 18, 18, 19, 16, // LEFT
-    20, 21, 22, 22, 23, 20  // BOTTOM
-};
-
-void MyApp::createBufferObjects() {
-    GLuint boId[2];
-
-    glGenVertexArrays(1, &VaoId);
-    glBindVertexArray(VaoId);
-    {
-        glGenBuffers(2, boId);
-        glBindBuffer(GL_ARRAY_BUFFER, boId[0]);
-        {
-            glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-
-            glEnableVertexAttribArray(POSITION);
-            glVertexAttribPointer(POSITION, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-                reinterpret_cast<void*>(0));
-
-            glEnableVertexAttribArray(COLOR);
-            glVertexAttribPointer(
-                COLOR, 4, GL_FLOAT, GL_FALSE, sizeof(Vertex),
-                reinterpret_cast<GLvoid*>(sizeof(Vertices[0].XYZW)));
-        }
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, boId[1]);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices,
-            GL_STATIC_DRAW);
-    }
-    glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    glDeleteBuffers(2, boId);
-}
-
-void MyApp::destroyCameras() {
-    for (int i = 0; i < cameras.size(); i++) {
-        delete cameras[i];
-    }
-}
-
-void MyApp::destroyBufferObjects() {
-    glBindVertexArray(VaoId);
-    glDisableVertexAttribArray(POSITION);
-    glDisableVertexAttribArray(COLOR);
-    glDeleteVertexArrays(1, &VaoId);
-    glBindVertexArray(0);
-}
 
 ///////////////////////////////////////////////////////////////////////// MESHES
 
@@ -201,7 +95,7 @@ mgl::Mesh* MyApp::getMesh(std::string mesh_dir, std::string mesh_file) {
 
 void MyApp::createSceneGraph() {
     std::string dir = "../assets/";
-    mgl::Mesh* enviromentMesh = getMesh(dir, "enviroment_vn.obj");
+    mgl::Mesh* enviromentMesh = getMesh(dir, "square_vn.obj");
     mgl::Mesh* squareMesh = getMesh(dir, "square_vn.obj");
     mgl::Mesh* parallelogramMesh = getMesh(dir, "parallelogram_vn.obj");
     mgl::Mesh* sTriangle1Mesh = getMesh(dir, "small_triangle_1_vn.obj");
@@ -284,6 +178,12 @@ void MyApp::createCameras() {
     cameras.push_back(camera2);
 
     cameras[currCamera]->bind();
+}
+
+void MyApp::destroyCameras() {
+    for (int i = 0; i < cameras.size(); i++) {
+        delete cameras[i];
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////// ANIMATION
@@ -408,7 +308,6 @@ void MyApp::scrollCallback(GLFWwindow* win, double xoffset, double yoffset) {
 
 void MyApp::windowCloseCallback(GLFWwindow* win) { 
     destroyCameras();
-    destroyBufferObjects(); 
 }
 
 void MyApp::windowSizeCallback(GLFWwindow* win, int winx, int winy) {
